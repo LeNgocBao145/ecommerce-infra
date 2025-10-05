8a507de1500e9f9683e101b00dc421bb9a857cf3
lengocbao145 <23127155@student.hcmus.edu.vn>
2025-10-05 11:33:06 +0700

    feat(variables): Define variables for providers config

---
+++ modules/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type = string
+  description = "Default AWS region"
+}
+
+variable "gcp_region" {
+  type = string
+  description = "Default GCP region"
+}
+
+variable "gcp_project_id" {
+  type = string
+  description = "Google Cloud Project ID"
+}
